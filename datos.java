ddac0b28adbc1c3ec2c1ad4908a0aa844fad2342
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2021-10-03 08:19:41 -0600

    Agregación de los lugares, y agregación de creditos

--- datos.java
+++ datos.java
@@ -9,29 +9,12 @@ import java.util.Scanner;
  */
 public class datos {
     Scanner sc = new Scanner(System.in);
-    ArrayList<String> Zona1 = new ArrayList<>();
-    ArrayList<String> Zona2 = new ArrayList<>();
     ArrayList<String> Zona3 = new ArrayList<>();
-    ArrayList<String> Zona4 = new ArrayList<>();
-    ArrayList<String> Zona5 = new ArrayList<>();
     ArrayList<String> Zona6 = new ArrayList<>();
-    ArrayList<String> Zona7 = new ArrayList<>();
-    ArrayList<String> Zona8 = new ArrayList<>();
     ArrayList<String> Zona9 = new ArrayList<>();
-    ArrayList<String> Zona10 = new ArrayList<>();
-    ArrayList<String> Zona11 = new ArrayList<>();
     ArrayList<String> Zona12 = new ArrayList<>();
-    ArrayList<String> Zona13 = new ArrayList<>();
     ArrayList<String> Zona14 = new ArrayList<>();
-    ArrayList<String> Zona15 = new ArrayList<>();
-    ArrayList<String> Zona16 = new ArrayList<>();
-    ArrayList<String> Zona17 = new ArrayList<>();
     ArrayList<String> Zona18 = new ArrayList<>();
-    ArrayList<String> Zona19 = new ArrayList<>();
-    ArrayList<String> Zona20 = new ArrayList<>();
-    ArrayList<String> Zona21 = new ArrayList<>();
-    ArrayList<String> Zona22 = new ArrayList<>();
-    ArrayList<String> Zona23 = new ArrayList<>();
     /**
      * arranque();
     * Este proceso se usa para poder agregar e iniciar las listas de Arraylist que en un inicio estan vacias y 
@@ -39,7 +22,11 @@ public class datos {
      */
     public void arranque() {
         Zona12.add("- Vical, de, Av. Petapa 48-01, Cdad. de Guatemala 01012, +502 2422 6400");
-        Zona9.add("- Recipa, 3 Avenida 2-16 Zona 9 Ciudad de Guatemala, 01009, +502 2491 5050 "); // centros de reciclaje 
+        Zona9.add("- Recipa, 3 Avenida 2-16 Zona 9 Ciudad de Guatemala, 01009, +502 2491 5050 "); 
+        Zona14.add("- Scrapex, 5ta Avenida 5-55, Edificio Europlaza, Torre 1 Nivel 2 Zona 14 ciudad de Guatemala, +502 2386 9999");
+        Zona6.add("- La Joya, 1 Calle 1-85 Zona 6 Col Alamos San Miguel Petapa, + 502 2448 1286 ");
+        Zona18.add("- Red Ecologica, Kilómetro 8 Carretera al Atllántico, Zona 18, Ciudad de Guatemala, +502 2301-1500");
+        Zona3.add("- Interfisa, 7a. Avenida 39-26, zona 3, Guatemala, +502 5834-5723"); // centros de reciclaje 
     }
     /**
     * Zone()
@@ -56,29 +43,11 @@ public class datos {
     * Estos preocesos desliegan en lista los elementos de toda la lista a la que se le solicita, eso queiere decir
         que regresa lo que es la informaicón de toda la zona solicitada
     */
-    public void zone1() {
-        for(int i = 0; i < Zona1.size(); i++) {
-            System.out.println(Zona1.get(i));
-        }
-    }
-    public void zone2() {
-        for(int i = 0; i < Zona2.size(); i++) {
-            System.out.println(Zona2.get(i));
-        }
-    }
+    
+    
     public void zone3() {
         for(int i = 0; i < Zona3.size(); i++) {
             System.out.println(Zona3.get(i));
-        }
-    }
-    public void zone4() {
-        for(int i = 0; i < Zona4.size(); i++) {
-            System.out.println(Zona4.get(i));
-        }
-    }
-    public void zone5() {
-        for(int i = 0; i < Zona5.size(); i++) {
-            System.out.println(Zona5.get(i));
         }
     }
     public void zone6() {
@@ -86,29 +55,9 @@ public class datos {
             System.out.println(Zona6.get(i));
         }
     }
-    public void zone7() {
-        for(int i = 0; i < Zona7.size(); i++) {
-            System.out.println(Zona7.get(i));
-        }
-    }
-    public void zone8() {
-        for(int i = 0; i < Zona8.size(); i++) {
-            System.out.println(Zona8.get(i));
-        }
-    }
     public void zone9() {
         for(int i = 0; i < Zona9.size(); i++) {
             System.out.println(Zona9.get(i));
-        }
-    }
-    public void zone10() {
-        for(int i = 0; i < Zona10.size(); i++) {
-            System.out.println(Zona10.get(i));
-        }
-    }
-    public void zone11() {
-        for(int i = 0; i < Zona11.size(); i++) {
-            System.out.println(Zona11.get(i));
         }
     }
     public void zone12() {
@@ -116,59 +65,14 @@ public class datos {
             System.out.println(Zona12.get(i));
         }
     }
-    public void zone13() {
-        for(int i = 0; i < Zona13.size(); i++) {
-            System.out.println(Zona13.get(i));
-        }
-    }
     public void zone14() {
         for(int i = 0; i < Zona14.size(); i++) {
             System.out.println(Zona14.get(i));
         }
     }
-    public void zone15() {
-        for(int i = 0; i < Zona15.size(); i++) {
-            System.out.println(Zona15.get(i));
-        }
-    }
-    public void zone16() {
-        for(int i = 0; i < Zona16.size(); i++) {
-            System.out.println(Zona16.get(i));
-        }
-    }
-    public void zone17() {
-        for(int i = 0; i < Zona17.size(); i++) {
-            System.out.println(Zona17.get(i));
-        }
-    }
     public void zone18() {
         for(int i = 0; i < Zona18.size(); i++) {
             System.out.println(Zona18.get(i));
-        }
-    }
-    public void zone19() {
-        for(int i = 0; i < Zona19.size(); i++) {
-            System.out.println(Zona19.get(i));
-        }
-    }
-    public void zone20() {
-        for(int i = 0; i < Zona20.size(); i++) {
-            System.out.println(Zona20.get(i));
-        }
-    }
-    public void zone21() {
-        for(int i = 0; i < Zona21.size(); i++) {
-            System.out.println(Zona21.get(i));
-        }
-    }
-    public void zone22() {
-        for(int i = 0; i < Zona22.size(); i++) {
-            System.out.println(Zona22.get(i));
-        }
-    }
-    public void zone23() {
-        for(int i = 0; i < Zona23.size(); i++) {
-            System.out.println(Zona23.get(i));
         }
     }
 }
