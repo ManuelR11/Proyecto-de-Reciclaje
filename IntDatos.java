b7ac4af13d79ffd1cb68b60adec014b237716706
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2021-11-21 17:41:01 -0600

    Intragación y resolución

---
+++ IntDatos.java
@@ -0,0 +1,10 @@
+public interface IntDatos {
+    public void arranque();
+    public int zone();
+    public void zone3();
+    public void zone6();
+    public void zone9();
+    public void zone12();
+    public void zone14();
+    public void zone18();
+}
